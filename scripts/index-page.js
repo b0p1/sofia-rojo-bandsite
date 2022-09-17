49db02d62a550159a870f60a4f45ae263af49294
b0p1 <sofiarojo09@gmail.com>
2022-09-17 15:09:58 -0400

    add static comments and form to main page (no styling)

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,18 +1,18 @@
 let comments = [
     {
-        icon: 'img',
+        avatar: '',
         name:'Connor Walton',
         date:'02/17/2021',
         comment:'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
     },
     {
-        icon: 'img',
+        avatar: '',
         name:'Emilie Beach',
         date:'01/09/2021',
         comment:'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
     },
     {
-        icon: 'img',
+        avatar: '',
         name:'Miles Acosta',
         date:'12/20/2020',
         comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
@@ -35,11 +35,28 @@ function createCommentCard(commentData) {
     commentList.appendChild(commentCard);
     
     // icon section
+    let avatar = document.createElement('img');
+    avatar.classList.add('comment__avatar');
+    avatar.innerText = commentData.avatar;
+    commentCard.appendChild(avatar);
 
-    
     // name section
     let nameLabel = document.createElement('h3');
     nameLabel.classList.add('comment__name-label');
     nameLabel.innerText = commentData.name;
     commentCard.appendChild(nameLabel);
+
+    // date section 
+    let date = document.createElement('h4');
+    date.classList.add('comment__date');
+    date.innerText = commentData.date;
+    commentCard.appendChild(date);
+
+    // comment section
+    let comment = document.createElement('p');
+    comment.classList.add ('comment__content');
+    comment.innerText = commentData.comment;
+    commentCard.appendChild(comment);
+
+    return commentCard
 }
