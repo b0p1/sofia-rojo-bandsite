dfc0e0276a4c9084440bc6e94220af644e8c1965
b0p1 <sofiarojo09@gmail.com>
2022-09-20 18:33:31 -0400

    get prewritten comments through API instead of array... still can't upload comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,73 +1,52 @@
-let commentsArray = [
-  {
-    avatar: "",
-    name: "Connor Walton",
-    date: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    avatar: "",
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    avatar: "",
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
+const usersURL =
+  "https://project-1-api.herokuapp.com/comments?api_key=8613e1fd-39c9-441a-989c-bdabccc121ba";
 
 let commentSection = document.querySelector(".comment-section");
 let commentList = document.querySelector(".static-comments");
 let commentForm = document.querySelector("#commentForm");
 
+axios.get(usersURL).then((response) => {
+  response.data.forEach(displayComment);
+});
+
+function displayComment(element) {
+  let commentCard = createCommentCard(element);
+  commentList.appendChild(commentCard);
+}
+
 commentForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  let avatar = "";
   let name = e.target.fullName.value;
   let date = new Date();
+  let formattedDate = formatDate(date);
 
+  axios
+    .post(usersURL, {
+      avatar: avatar,
+      name: name,
+      date: date,
+      timestamp: formattedDate,
+    })
+    .then((response) => {
+      createCommentCard(response.data);
+      commentForm.reset();
+    });
+
+  // e.target.reset();
+});
+
+function formatDate(date) {
   let timeStampDate = String(date.getDate()).padStart(2, "0");
   let timeStampMonth = String(date.getMonth() + 1).padStart(2, "0");
   let timeStampYear = date.getFullYear();
+
   let timeStampArray =
     timeStampMonth + "/" + timeStampDate + "/" + timeStampYear;
-  console.log(timeStampArray);
 
-  let comment = e.target.comment.value;
-
-  let formComments = {
-    avatar: "",
-    name: name,
-    date: timeStampArray,
-    comment: comment,
-  };
-
-  commentsArray.unshift(formComments);
-  console.log(commentsArray);
-
-  displayComment();
-
-  e.target.reset();
-});
-
-function displayComment() {
-  commentList.innerHTML = "";
-
-  for (let i = 0; i < commentsArray.length; i++) {
-    let commentData = commentsArray[i];
-    let commentCard = createCommentCard(commentData);
-    console.log(commentCard);
-    commentList.appendChild(commentCard);
-  }
+  return timeStampArray;
 }
-
-displayComment();
 
 function createCommentCard(commentData) {
   let commentCard = document.createElement("div");
@@ -81,7 +60,7 @@ function createCommentCard(commentData) {
   // icon section
   let avatar = document.createElement("span");
   avatar.classList.add("comment__avatar");
-  avatar.innerText = commentData.avatar;
+  avatar.innerText = commentData.avatar || "";
   commentTitles.appendChild(avatar);
 
   // name section
@@ -93,7 +72,7 @@ function createCommentCard(commentData) {
   // date section
   let date = document.createElement("h4");
   date.classList.add("comment__date");
-  date.innerText = commentData.date;
+  date.innerText = formatDate(new Date(commentData.timestamp));
   commentTitles.appendChild(date);
 
   // comment section
